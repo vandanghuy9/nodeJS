5362747d83e747e994590c6649f37a58301d77c2
vandanghuy9 <106438610+vandanghuy9@users.noreply.github.com>
2022-09-14 14:54:30 +0700

    Create 2_Global-var.js

---
+++ 2_Global-var.js
@@ -0,0 +1,14 @@
+// GLOBALS - no WINDOW OBJECT
+// __dirname: path to current directory
+// __filename: file name
+// require: to use modules
+// module: info bout current module
+
+// process - an global var give in4 bout env where 
+//program is execute
+
+console.log(__dirname);
+console.log(process);
+setInterval(() => {
+    console.log("hello");
+},1000);
