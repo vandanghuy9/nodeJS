7481020b6038694a1164768cbfb412292a31e332
vandanghuy9 <106438610+vandanghuy9@users.noreply.github.com>
2022-09-30 22:37:30 +0700

    Update app.js

--- authorize/app.js
+++ authorize/app.js
@@ -7,19 +7,28 @@ const {products} = require('./data');
 //set up static and middleware
 // req => middleware => res
 const logger = require('./logger'); // create separate file for middleware
-
-app.use(logger); //use logger on every routes
+const morgan = require('morgan'); //install morgan modules
+const { resourceLimits } = require('worker_threads');
+app.use(morgan('tiny'));
+ //use logger on every routes
 //app.use can be used when want to apply on every route
-app.get('/',(req,res) => {
+app.get('/',(req,res) => { //homepage
     res.send("home");
 })
-app.get('/about',(req,res) => {
+app.get('/about',(req,res) => { //about
     res.send("About");
 })
-app.get('/api/products',(req,res) => {
+app.get('/api/products',(req,res) => { //localhost:5000/api/products?user=john
+    console.log(req.user);
     res.send("Products");
 })
 app.listen(5000, function(){
 
     console.log("server is good");
 });
+
+// result 
+// server is good
+// GET / 200 4 - 9.178 ms
+// undefined
+// GET /api/products?user=john 304 - - 3.675 ms
